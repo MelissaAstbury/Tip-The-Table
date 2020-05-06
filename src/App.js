c94fecb84a911977eac4873882cc2367e43054d3
Melissa Astbury <melissajastbury@gmail.com>
2020-05-06 18:51:31 +0100

    input the 2 inputs and added in the functionality so the numbers entered by the user iscalculated

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
-function App() {
+const App = () => {
+  const [billAmount, setBillAmount] = useState(0);
+  const [people, setPeople] = useState(0);
+  const [tip, setTip] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    let calculatedTip = (billAmount * 15) / 100;
+    let totalTip = calculatedTip / people;
+    setTip(totalTip);
+  };
+
   return (
-    <div>
-      <p>hello tip</p>
-    </div>
+    <form onSubmit={onSubmit}>
+      <div>
+        <p>How much is your bill?</p>
+        <input
+          type="number"
+          value={billAmount}
+          required
+          min="10"
+          onChange={(e) => {
+            setBillAmount(e.target.value);
+          }}
+        />
+        <p>How many people?</p>
+        <input
+          type="number"
+          value={people}
+          required
+          min="1"
+          onChange={(e) => {
+            setPeople(e.target.value);
+          }}
+        />
+        <button type="submit">Calculate!</button>
+
+        <p>Your should tip: £{tip}</p>
+      </div>
+    </form>
   );
-}
+};
 
 export default App;
